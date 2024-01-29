ec48edb7da4ec6eb47d291b4c07bcbb4bb54ddc6
RayyanZaid <rayyanzaid0401@gmail.com>
2024-01-29 10:45:58 -0800

    Not using .ics library Function to get current time

---
+++ Experimenting/getCurrentTime.py
@@ -0,0 +1,5 @@
+import time
+
+current_time = time.ctime()
+
+print(current_time)
--- Experimenting/script_for_ics_parsing.py
+++ Experimenting/script_for_ics_parsing.py
@@ -1,28 +1,25 @@
-from ics import Calendar
-
-
 def parse_ics(file_path):
+
+    calendar : str
+
     with open(file_path, 'r') as file:
-        calendar = Calendar(file.read())
+        calendar = file.read()
 
-    events_info = []
-    for event in calendar.events:
-        event_info = {
-            "summary": event.name,
-            "start": event.begin.datetime,
-            "end": event.end.datetime,
-            "location": event.location,
-            "description": event.description
-        }
-        events_info.append(event_info)
+    
+    calendarArray = calendar.split('\n')
 
-    return events_info
+    currentLineNumber = 0
+    numLines = len(calendarArray)
+    while(currentLineNumber < numLines):
+
+        eachLine = calendarArray[currentLineNumber]
+
+        # print(eachLine)
+
+        if(eachLine == "BEGIN:VEVENT"):
+            print("YAY")
+
+        currentLineNumber += 1
 
 
-# Example usage
-file_path = 'Fall2023.ics'
-event_details = parse_ics(file_path)
-for detail in event_details:
-    print(detail['summary'])
-    print(detail['start'])
-    print(detail['end'])
+parse_ics("Fall2023.ics")

b973579db68a706c618060b1ff5c0c3a03a54df3
RayyanZaid <rayyanzaid0401@gmail.com>
2024-02-02 00:32:49 -0800

    made course constructor

--- ScheduleScreen/Course.py
+++ ScheduleScreen/Course.py
@@ -1,4 +1,34 @@
 class Course:
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, className : str, courseNumber : str, teacherName : str, locationInfo : dict, timeInfo : dict) -> None:
+        
+        self.className = className
+        self.courseNumber = courseNumber
+        self.teacherName = teacherName
+
+
+
+
+        # locationInfo = {
+        
+        #  buildingName : "",
+        #  roomNumber : "",
+        #  geoLocation : [],
+        
+        # }
+
+
+        self.locationInfo = locationInfo
+
+
+        # timeInfo = {
+        
+        # dateEnd
+        # dateStart
+        # daysOfWeek
+        # startTime
+        # endTime
+        
+        # }
+        
+        self.timeInfo = timeInfo

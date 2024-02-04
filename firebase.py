214c2f9fd57e23cccc9db6af7b24b0a010334ad8
RayyanZaid <rayyanzaid0401@gmail.com>
2024-02-03 21:09:31 -0800

    requirements updated

--- firebase.py
+++ firebase.py
@@ -3,24 +3,14 @@ from firebase_admin import credentials, firestore, auth
 
 
 # Use a service account.
-cred = credentials.Certificate('credentials.json')
+cred =  credentials.Certificate('credentials.json')
 
 app = firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
 
-try:
-
-    user = auth.create_user(
-        phone_number='+16265342234'
-        
-    )
-
-
-except Exception as e:
-    print(e)
 # print(db)
 
-# doc_ref = db.collection("users").document("rzaid")
-# doc_ref.set({"first": "Rayyan", "last": "Zaid", "born": 2003})
+doc_ref = db.collection("users").document("rzaid")
+doc_ref.set({"first": "Rayyan", "last": "Zaid", "born": 2003})

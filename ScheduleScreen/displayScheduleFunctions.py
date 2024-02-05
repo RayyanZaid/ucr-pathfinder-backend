f252f104e6a91397369e6fb10cad63df8a76f8c1
RayyanZaid <rayyanzaid0401@gmail.com>
2024-02-05 00:58:05 -0800

    separated by day

--- ScheduleScreen/displayScheduleFunctions.py
+++ ScheduleScreen/displayScheduleFunctions.py
@@ -16,12 +16,59 @@ def getSchedule(uid):
 
         docInfo = doc.to_dict()
 
-        print(docInfo["schedule"])
+        # print(docInfo["schedule"])
         
         scheduleDictionary = docInfo["schedule"]
     
     else:
         print("No doc")
 
+
+    scheduleDictionary = getCurrentDayClasses(scheduleDictionary)
     return scheduleDictionary
+
+
+# 0 -- Monday
+# 1 -- Tuesday
+# 4 -- Friday
+
+numberToDay = {
+    0 : "MO",
+    1 : "TU",
+    2 : "WE",
+    3 : "TH",
+    4 : "FR", 
+}
+def getCurrentDayClasses(scheduleDictionary):
+
+    todaysDayNumber = datetime.today().weekday()
+
+    todaysDayName = numberToDay[todaysDayNumber]
+    # Display all classes that have BYDAYs on 'todays_day_number'
+
+    i = 0
+    while i < len(scheduleDictionary):
+
+        eachCourse = scheduleDictionary[i]
+        daysArray = eachCourse['timeInfo']['rRule']['BYDAY']
+
+
+        # if no days match, delete the course
+
+        isToday : bool = False
+
+        for eachDay in daysArray:
+            
+            if eachDay == todaysDayName:
+                isToday = True
+                break
+        
+        if not isToday:
+            scheduleDictionary.pop(i)
+        else:
+            i += 1
+        
+
     
+    return scheduleDictionary
+

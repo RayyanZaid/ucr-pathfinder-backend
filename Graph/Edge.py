12b372fb56459aa180543f2cd62168473219ed8d
william1423 <williamhuang4321@gmail.com>
2024-02-14 12:52:53 -0800

    formatting

--- Graph/Edge.py
+++ Graph/Edge.py
@@ -8,10 +8,12 @@ class Edge:
         self.n2 = n2
 
     def setDistance(self):
-        coord1 = (self.n1.location[0], self.n1.location[1]) #needs to be in the formant (lat,long)
+        coord1 = (self.n1.location[0], self.n1.location[1]) #needs to be in the format (lat,long)
         coord2 = (self.n2.location[0], self.n2.location[1])
         self.length = distance.distance(coord1, coord2, ellipsoid='WGS-84').feet
+
     def setTime(self, time : float):
         self.time = geopy.units.miles(feet=self.length) * 2.5
+
     def printValues(self):
         print(self.length + ' ' + self.time)

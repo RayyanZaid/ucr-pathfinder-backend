9a7d7c1d7af08a6b950cf2d316f0eb60f38427e6
william1423 <williamhuang4321@gmail.com>
2024-02-14 12:48:04 -0800

    starting edge class

--- Graph/Edge.py
+++ Graph/Edge.py
@@ -1,11 +1,17 @@
+import geopy
 from Node import Node
+from geopy import distance
 
 class Edge:
-    def __init__(self, n1 : Node, n2 : Node, ):
+    def __init__(self, n1 : Node, n2 : Node):
         self.n1 = n1
         self.n2 = n2
 
-#we want to use a geodetic/ellipsiod formula because the coordinates we get from google are based on WGS84, the world geodetic system
-#i dont want to reinvent the wheel on geodetic caluculations so i will be using geopy
-
-#if __name__ == '__main__':
+    def setDistance(self):
+        coord1 = (self.n1.location[0], self.n1.location[1]) #needs to be in the formant (lat,long)
+        coord2 = (self.n2.location[0], self.n2.location[1])
+        self.length = distance.distance(coord1, coord2, ellipsoid='WGS-84').feet
+    def setTime(self, time : float):
+        self.time = geopy.units.miles(feet=self.length) * 2.5
+    def printValues(self):
+        print(self.length + ' ' + self.time)

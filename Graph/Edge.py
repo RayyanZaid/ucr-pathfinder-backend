6a81cd9796dd90cb40fd4ab97f69a7d33b8b759f
RayyanZaid <rayyanzaid0401@gmail.com>
2024-02-14 19:19:53 -0800

    Fixed time conversion

--- Graph/Edge.py
+++ Graph/Edge.py
@@ -1,19 +1,50 @@
 import geopy
 from Node import Node
 from geopy import distance
+from Node import AutomateNodeCreation
+
+
+MPH_WALKING_SPEED = 2.5
+MILE_TO_FEET = 5280
+
+FEET_PER_MINUTE = (MPH_WALKING_SPEED * MILE_TO_FEET) / 60
 
 class Edge:
     def __init__(self, n1 : Node, n2 : Node):
         self.n1 = n1
         self.n2 = n2
+        self.length : int
+        self.time : float
 
     def setDistance(self):
         coord1 = (self.n1.location[0], self.n1.location[1]) #needs to be in the format (lat,long)
         coord2 = (self.n2.location[0], self.n2.location[1])
         self.length = distance.distance(coord1, coord2, ellipsoid='WGS-84').feet
 
-    def setTime(self, time : float):
-        self.time = geopy.units.miles(feet=self.length) * 2.5
+    def setTime(self):
+        
+        self.time = self.length / FEET_PER_MINUTE
 
     def printValues(self):
-        print(self.length + ' ' + self.time)
+        print(f"Distance: {self.length} feet")
+        print(f"Time: {self.time} minutes")
+
+
+if __name__ == "__main__":
+
+    nodeCreator = AutomateNodeCreation("./Experimenting/GoogleEarth/test.kml")
+
+    nodes : list[Node] = nodeCreator.createNodesFromKml()
+
+
+    testEdge : Edge = Edge(nodes[0] , nodes[1])
+    testEdge.setDistance()
+    testEdge.setTime()
+    testEdge.printValues()
+    # for eachNode in nodes:
+
+    #     print(f"Name: {eachNode.name}")
+    #     print(f"Number: {eachNode.number}")
+    #     print(f"Location: {eachNode.location}")
+    #     print(f"Type: {eachNode.type}")
+    #     print(f"Neighbors: {eachNode.neighbors}")

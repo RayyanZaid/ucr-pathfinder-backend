d96cc7112fdc1225461f7aa66db8b67ce8e6f9b4
William <williamhuang4321@gmail.com>
2024-02-20 15:41:01 -0800

    shortest path without priority queue

--- Graph/Navigation.py
+++ Graph/Navigation.py
@@ -1,9 +1,12 @@
-
+import copy
 from geopy import distance
 from Graph import Graph
-
 from Node import NodeType
 
+MPH_WALKING_SPEED = 3
+MILE_TO_FEET = 5280
+MINUTES_IN_HOUR = 60
+FEET_PER_MINUTE = (MPH_WALKING_SPEED * MILE_TO_FEET) / MINUTES_IN_HOUR
 
 ucr_graph : Graph = Graph("Graph/UCR_PathFinder_Coordinates.kml")
 ucr_graph.createNodes()
@@ -11,70 +14,49 @@ ucr_graph.createEdges()
 
 
 class Navigation:
-
-
-
             # [latitude , longitude , altitude]
     def __init__(self, location : list[float], destinationBuildingName : str) -> None:
         
         self.location : list[int] = location
         self.destinationBuildingName : str = destinationBuildingName
-
         self.closestNodeIdToUser : str
-
-
         self.sourceNodeID : str
-
         self.destinationNodeIDs : list[str]
-
+        self.totalCost = dict[str : float]
+        self.vertexPaths = dict[str: list[str]]
+        self.finished = list[str]
+        self.current = list[str]
+        self.pathToDestination = list[str]
 
     def setClosestNodeToUser(self):
-
         closestNodeID : str = None
         closestNodeDistance : float = float('inf')
 
         for nodeID, nodeObject in ucr_graph.nodeIdToObject.items():
-
-
             coord1 = (self.location[0], self.location[1]) #needs to be in the format (lat,long)
             coord2 = (nodeObject.location[0], nodeObject.location[1])
             euclidDistance = distance.distance(coord1, coord2, ellipsoid='WGS-84').feet
 
             if euclidDistance < closestNodeDistance:
-
                 closestNodeID = nodeID
                 closestNodeDistance = euclidDistance
-        
-
+    
         self.sourceNodeID = closestNodeID
-            
 
-            
-
-
-
-
-    # Returns a list of node ids representing entrances of building
-                
+    # Returns a list of node ids representing entrances of building           
     def setBuildingNodes(self) -> list[str]:
-        
         buildingNodeIDs : list[str] = []
-
 
         for nodeID, nodeObject in ucr_graph.nodeIdToObject.items():
 
             if nodeObject.type == NodeType.BUILDING:
-
                 nodeBuildingName = nodeObject.name
 
                 if nodeBuildingName == self.destinationBuildingName:
-
                     buildingNodeIDs.append(nodeID)
-
 
         self.destinationNodeIDs = buildingNodeIDs
 
-    
     def similarityScoring(self):
 
         # might implement later
@@ -86,17 +68,15 @@ class Navigation:
 
         for edge in ucr_graph.nodeIdToObject[id].edges:
             if edge.n1.nodeID == id:
-                adjacents.append(edge.n1.nodeID)
-            elif edge.n2.nodeID == id:
                 adjacents.append(edge.n2.nodeID)
+            elif edge.n2.nodeID == id:
+                adjacents.append(edge.n1.nodeID)
             else:
                 print("Error in getting adjacent nodes in navigation class")
 
         return adjacents
     
     def getDistance(self, cur : str, adj : str):
-        
-        # what you had before: 
         edges = ucr_graph.nodeIdToObject[cur].edges
         for edge in  edges:
             if adj == edge.n1.nodeID or adj == edge.n2.nodeID:
@@ -126,17 +106,26 @@ class Navigation:
                     self.vertexPaths[each].append(id) # update path of neighbor
             else:
                 cost = self.totalCost[id] + self.getDistance(id, each)
-                path = self.vertexPaths[id]
+                path = copy.copy(self.vertexPaths[id])
                 path.append(each)
-                
+
                 self.current.append(each)
                 self.totalCost[each] = cost
                 self.vertexPaths[each] = path
-                print()
 
         self.finished.append(id)
         self.current.remove(id)
 
+    def findEdges(self, nodes : list[str]):
+        result = []
+        for i in range(len(nodes) - 1):
+            cur = nodes[i]
+            next = nodes[i + 1]
+            edges = ucr_graph.nodeIdToObject[cur].edges
+            for edge in edges:
+                if edge.n1.nodeID == next or edge.n2.nodeID == next:
+                    result.append(edge)
+        return result
     """
         Returns a dictionary that looks like this:
         {
@@ -159,6 +148,7 @@ class Navigation:
             selected = self.findMinID()                     #find unfinished node with shortest distance
             if selected == destinationNodeID:
                 found = 1
+                continue
             self.calculate(selected)
             if not self.current:
                 found = 1
@@ -166,48 +156,38 @@ class Navigation:
                 return
 
         self.pathToDestination = self.vertexPaths[destinationNodeID] # destination path
-        return self.totalCost[destinationNodeID]
+        return self.totalCost[destinationNodeID], self.vertexPaths[destinationNodeID]
     
-
-
-
     def getShortestPathNodesAndEdges(self) -> dict[str , list]:
-
         navigationDictionary = {
-
             "nodes" : [],
             "edges" : [],
-
+            "length" : int,
+            "time" : float
         }
 
         sourceNodeID = self.sourceNodeID
-
         destinationNodeIDs = self.destinationNodeIDs
-
-
         minLength = float('inf')
         minTime = float('inf')
         minNodes = []
         minEdges = []
-        for eachDestinationNodeID in destinationNodeIDs:
 
-            nodes, edges, length = self.shortestPathAlgorithm(sourceNodeID, eachDestinationNodeID)
+        for eachDestinationNodeID in destinationNodeIDs:
+            length, nodes = self.shortestPathAlgorithm(sourceNodeID, eachDestinationNodeID) # edges
 
             if length < minLength:
                 minLength = length
-                minTime = 0 # CHANGE THIS TO FORMULA length --> time
+                minTime = minLength / FEET_PER_MINUTE
                 minNodes = nodes
-                minEdges = edges
+                minEdges = self.findEdges(minNodes)
                 
-
-
         navigationDictionary['nodes'] = minNodes
         navigationDictionary['edges'] = minEdges
         navigationDictionary['length'] = minLength
         navigationDictionary['time'] = minTime
         
         return navigationDictionary
-
 
 if __name__ == "__main__":
 

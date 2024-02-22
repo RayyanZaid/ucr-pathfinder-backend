a5e5a39da9304ac9c81fd0b745bc0bbd00662fae
RayyanZaid <rayyanzaid0401@gmail.com>
2024-02-21 17:44:35 -0800

    test case 5

--- Testing/GraphImplementation/TestNavigation.py
+++ Testing/GraphImplementation/TestNavigation.py
@@ -45,38 +45,53 @@ from Graph.Navigation import Navigation
 
 
 
-# Test 3
-# Testing Shortest Path (some bugs)
+# # Test 3
+# # Testing Shortest Path (some bugs)
 
-userLocation22 = [33.97551741560539, -117.3280488150658, 318.0153092198389]
+# userLocation22 = [33.97551741560539, -117.3280488150658, 318.0153092198389]
+# destinationBuildingName = "Materials Sci and Engineering"
+# navigationObject : Navigation = Navigation(userLocation22 , destinationBuildingName)
+# navigationObject.setClosestNodeToUser() # Should be 22
+# print(navigationObject.sourceNodeID == '22')
+
+
+# navigationObject.setBuildingNodes()
+# print(navigationObject.destinationNodeIDs == ['9', '12'])
+
+
+# navigationDictionary = navigationObject.getShortestPathNodesAndEdges()
+# print()
+
+
+# # Test 4
+
+# # Shortest Path (some bugs)
+
+# userLocationNode30 = [33.97571667778362, -117.3286881329295,317.4348304680136] 
+# destinationBuildingName = "Materials Sci and Engineering"
+# navigationObject : Navigation = Navigation(userLocationNode30 , destinationBuildingName)
+# navigationObject.setClosestNodeToUser() # Should be 30, but it's going to 29 because they're the exact same coordinates
+# print(navigationObject.sourceNodeID == '30')
+
+
+# navigationObject.setBuildingNodes()
+# print(navigationObject.destinationNodeIDs == ['9', '12'])
+
+
+# navigationDictionary = navigationObject.getShortestPathNodesAndEdges()
+
+
+# Test 5
+
+# For some reason, this is going from 19 --> 23 --> 16,
+
+userLocation37 = [33.97488141666391, -117.3286340179911,318.9579109074609]
 destinationBuildingName = "Materials Sci and Engineering"
-navigationObject : Navigation = Navigation(userLocation22 , destinationBuildingName)
-navigationObject.setClosestNodeToUser() # Should be 22
-print(navigationObject.sourceNodeID == '22')
-
-
+navigationObject : Navigation = Navigation(userLocation37 , destinationBuildingName)
+navigationObject.setClosestNodeToUser() 
 navigationObject.setBuildingNodes()
-print(navigationObject.destinationNodeIDs == ['9', '12'])
-
-
 navigationDictionary = navigationObject.getShortestPathNodesAndEdges()
-print()
 
 
-# Test 4
 
-# Shortest Path (some bugs)
-
-userLocationNode30 = [33.97571667778362, -117.3286881329295,317.4348304680136] 
-destinationBuildingName = "Materials Sci and Engineering"
-navigationObject : Navigation = Navigation(userLocationNode30 , destinationBuildingName)
-navigationObject.setClosestNodeToUser() # Should be 30, but it's going to 29 because they're the exact same coordinates
-print(navigationObject.sourceNodeID == '30')
-
-
-navigationObject.setBuildingNodes()
-print(navigationObject.destinationNodeIDs == ['9', '12'])
-
-
-navigationDictionary = navigationObject.getShortestPathNodesAndEdges()
 print()

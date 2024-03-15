3f056537e00c6faedc865b3d2afc14820465e79a
RayyanZaid <rayyanzaid0401@gmail.com>
2024-03-15 15:44:41 -0700

    idk

--- server.py
+++ server.py
@@ -214,5 +214,5 @@ def hello_world():
     return {'name': "Rayyan", "major" : "Computer Science"}
 
 if __name__ == '__main__':
-    # Ensure the upload folder exists
+
     app.run(host="0.0.0.0")
